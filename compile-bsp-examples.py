311d58d398c7437fb0f7736800abce331edfc99e
Dan Whitman <dwhitman44@gmail.com>
2025-07-24 16:38:44 -0400

    * Adds a README. * Adds a script to compile all Tier 1 BSP examples, which is useful for assessing how HAL changes affect examples.

---
+++ compile-bsp-examples.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+""""
+Run with -h for usage information.
+"""
+from __future__ import annotations
+import argparse
+import os
+from dataclasses import dataclass, field
+from typing import List, Optional
+from colorama import init, Fore, Style
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description="Compiles all Tier BSP examples one at a time and stops if one fails. Should be run from `atsamd/boards/`.")
+parser.add_argument("--warnings", "-w", action="store_true",
+                    help="treat warnings as errors")
+args = parser.parse_args()
+
+
+@dataclass
+class Bsp:
+    name: str
+    special_examples: List[Example] = field(default_factory=lambda: [])
+
+    def example_from_filename(self, filename: str) -> Example:
+        (name, _) = os.path.splitext(os.path.basename(filename))
+
+        return next(iter([e for e in self.special_examples if e.name == name]), Example(name))
+
+    def examples(self) -> List[Example]:
+        return [self.example_from_filename(fname) for fname in sorted(os.listdir(os.path.join(self.name, "examples")))]
+
+
+@dataclass
+class Example:
+    name: str
+    features: Optional[List[str]] = None
+
+    def slug(self, bsp: Bsp) -> str:
+        return os.path.join(bsp.name, self.name)
+
+    def clippy(self, bsp: Bsp) -> bool:
+        wae = "RUSTFLAGS=\"-D warnings\"" if args.warnings else ""
+        os.chdir(bsp.name)
+        res = os.system(
+            f"{wae} cargo clippy {self.features_flag()} --example {self.name}")
+        os.chdir("..")
+        return res == 0
+
+    def features_flag(self) -> str:
+        if self.features is None:
+            return "--all-features"
+        else:
+            return f"--features \"{" ".join(self.features)}\""
+
+
+@dataclass
+class Result:
+    bsp: Bsp
+    example: Example
+    success: bool
+
+
+bsps = [
+    Bsp("atsame54_xpro"),
+    Bsp("samd11_bare"),
+    Bsp("feather_m0", [Example("adalogger", ["adalogger", "usb", "sdmmc"])]),
+    Bsp("feather_m4"),
+    Bsp("metro_m0"),
+    Bsp("metro_m4"),
+    Bsp("pygamer"),
+]
+
+# Compile results
+results = []
+for bsp in bsps:
+    for example in bsp.examples():
+        print(f"Compiling {example.slug(bsp)}...")
+        results.append(Result(bsp, example, example.clippy(bsp)))
+
+# Summary
+for res in results:
+    status_msg = Fore.GREEN + "ok" if res.success else Fore.RED + "FAILED"
+
+    print(f"{res.example.slug(res.bsp)}... {status_msg}" + Style.RESET_ALL)
